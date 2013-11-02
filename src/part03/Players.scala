13c1ceeb7fdb6ffb4a0c680ecadc56bce05a154d
programaker <programaker@gmail.com>
2013-11-02 21:47:52 -0200

    Never access tuple fields! Never!

--- src/part03/Players.scala
+++ src/part03/Players.scala
@@ -16,5 +16,5 @@ object Players {
 
   def scores = all.map(p => (p.name, p.score))
 
-  def bestScore = scores.sortBy(_._2).last
+  def bestScore = scores.sortBy { case (_, score) => score }.last
 }
